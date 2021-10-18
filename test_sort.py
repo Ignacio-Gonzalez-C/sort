c6496d76efeae450223dda2203c5ec007bfa479b
Ignacio Gonzalez <ignacio.gonzalez@pregrado.uoh.cl>
2021-10-18 20:59:30 -0300

    Se añaden los test de insertion-sort

--- test_sort.py
+++ test_sort.py
@@ -35,6 +35,38 @@ class TestSortingAlgorithms(unittest.TestCase):
         sort.quick_sort(a)
         self.assertEqual(a, [0, 0, 1, 5, 9, 9])
 
+    def test_insertion_sort_1(self):
+        """
+        Prueba caso general #1
+        """
+        a = [2, 2, 0, 1, 4, 2]
+        sort.inserion_sort(a)
+        self.assertEqual(a, [0, 1, 2, 2, 2, 4])
+
+    def test_insertion_sort_2(self):
+        """
+        Prueba caso general #2
+        """
+        a = [9, 9, 9, 9, 0, 0]
+        sort.inserion_sort(a)
+        self.assertEqual(a, [0, 0, 9, 9, 9, 9])
+
+    def test_insertion_sort_3(self):
+        """
+        Prueba caso general #3
+        """
+        a = [2, 8, 9, 4, 1, 50]
+        sort.inserion_sort(a)
+        self.assertEqual(a, [1, 2, 4, 8, 9, 50])
+    
+    def test_insertion_sort_3(self):
+        """
+        Prueba para lista de tamaño 1
+        """
+        a = [5]
+        sort.inserion_sort(a)
+        self.assertEqual(a, [5])
+
 
 if __name__ == '__main__':
     unittest.main()
